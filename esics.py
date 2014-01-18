6bbec9fac183ba80c15584c779b929141611e91f
Astalaseven <gatien.bovyn@gmail.com>
2014-01-18 14:31:33 +0100

    [fix] Hack for new pdf date format

--- esics.py
+++ esics.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import arrow
 from ics import Calendar, Event
-from os import listdir
 from pdftoics import xml_to_blocks, blocks_to_matrix_dict
 from scraperwiki import pdftoxml
 
 def list_pdf():
     '''Lists all pdfs in subdirectory'''
 
-    pdfs = []
+    pdfs = {}
+    directories = os.listdir("pdf/")
 
-    for files in listdir("pdf/."):
-        if files.endswith(".pdf"):
-            pdfs.append(files)
+    for i in directories:
+        pdfs[i] = []
+
+    for directory in directories:
+        for files in os.listdir("pdf/" + directory):
+            if files.endswith(".pdf"):
+                pdfs[directory].append(files)
 
     return pdfs
 
 
-def pdf_scrape(file):
+def pdf_scrape(pdf, directory):
     '''Convert pdf to xml'''
 
-    with open("pdf/" + file) as u:
+    with open("pdf/" + directory + "/" + pdf) as u:
         xml = pdftoxml(u.read())
 
     with open("xml/" + pdf + ".xml", "w") as w:
@@ -46,7 +51,7 @@ def ics_dates(xml):
     return begin, end
 
 
-def matrix_to_ics(matrix_dict, group, begin, end):
+def matrix_to_ics(matrix_dict, group, begin, end, directory):
 
     c = Calendar()
 
@@ -56,7 +61,14 @@ def matrix_to_ics(matrix_dict, group, begin, end):
 
     d = {0: 'Lundi', 1: 'Mardi', 2: 'Mercredi', 3: 'Jeudi', 4: 'Vendredi'}
 
-    begin = arrow.get("{} {}".format(begin, hours[0]), 'DD-MM-YYYY HH:mm')
+    # begin = arrow.get("{} {}".format(begin, hours[0]), 'DD-MM-YYYY HH:mm')
+    months = {"janvier":'01', "février":'02', "mars":'03', "avril":'04', "mai":'05', "juin":'06', 
+    "juillet":'07', "aout":'08', "septembre":'09', "octobre":'10', "novembre":'11', "décembre":'12'}
+
+    day, month, year = begin.split(" ")
+    begin = "{} {} {}".format(day, months[month], year)
+
+    begin = arrow.get("{} {}".format(begin, hours[0]), 'DD MM YYYY HH:mm')
 
 
     # for each day
@@ -83,30 +95,43 @@ def matrix_to_ics(matrix_dict, group, begin, end):
         # new day
         begin = begin.replace(days=+1)
 
-    with open("ics/" + group + ".ics", "w") as f:
+    if not os.path.exists("ics/" + directory):
+        os.makedirs("ics/" + directory)
+
+    with open("ics/" + directory + "/" + group + ".ics", "w") as f:
         f.writelines(c)
+
+def convert(directories, pdfs):
+
+    for directory in directories:
+
+        pdf = pdfs[directory]
+
+        for p in pdf:
+
+            pdf_scrape(p, directory)
+
+            with open("xml/" + p + ".xml") as r:
+
+                xml = r.readlines()
+
+            blocks = xml_to_blocks(xml)
+            matrix_dict = blocks_to_matrix_dict(blocks)
+            begin_date, end_date = ics_dates(xml)
+
+            # for each group
+            for group in matrix_dict.keys():
+
+                print(p, group)
+                matrix_to_ics(matrix_dict, group, begin_date, end_date, directory)
 
 
 if __name__ == '__main__':
 
     pdfs = list_pdf()
+    #print(pdfs)
+    directories = pdfs.keys()
 
     print("\nAll PDFs : " + str(pdfs) + "\n")
 
-    for pdf in pdfs:
-
-        pdf_scrape(pdf)
-
-        with open("xml/" + pdf + ".xml") as r:
-
-            xml = r.readlines()
-
-        blocks = xml_to_blocks(xml)
-        matrix_dict = blocks_to_matrix_dict(blocks)
-        begin_date, end_date = ics_dates(xml)
-
-        # for each group
-        for group in matrix_dict.keys():
-
-            print(pdf, group)
-            matrix_to_ics(matrix_dict, group, begin_date, end_date)
+    convert(directories, pdfs)
