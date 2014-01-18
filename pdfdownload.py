470d78589dcff76539cd9e26d8b47722e441272b
Astalaseven <gatien.bovyn@gmail.com>
2014-01-18 11:36:45 +0100

    [new] Create directory to store pdf

--- pdfdownload.py
+++ pdfdownload.py
@@ -1,14 +1,15 @@
+import os
 import requests
 from BeautifulSoup import BeautifulSoup
 
-user = "esi_id"
+username = "esi_id"
 password = "esi_pass"
 s = requests.Session()
 
 
-def authenticate(user, password, s):
+def authenticate(username, password, s):
 
-    payload = {'login': user, 'password': password}
+    payload = {'login': username, 'password': password}
     r = s.post("http://elearning.esi.heb.be/index.php", data=payload)
 
 
@@ -17,7 +18,7 @@ def get_timetable(s):
     url = "http://elearning.esi.heb.be/courses/R/"
     soup = BeautifulSoup(s.get(url).text)
 
-    timetable = url + soup.find("table").find("td").find("td").find("a")['href']
+    timetable = url + soup.find("table").find("td").find("td").findAll("a")[1]['href']
     soup = BeautifulSoup(s.get(timetable).text)
 
     return soup, timetable
@@ -34,41 +35,49 @@ def clean_url(url):
     return url
 
 
-def save_pdf(url, pdf, session):
+def save_pdf(url, directory, pdf, session):
 
-    with open("pdf/" + pdf, "wb") as p:
+    try:
+        with open("pdf/" + directory + "/" + pdf, "wb") as p:
 
-        r = session.get(url, stream=True)
+            r = session.get(url, stream=True)
 
-        for block in r.iter_content(1024):
+            for block in r.iter_content(1024):
 
-            if not block:
-                break
+                if not block:
+                    break
 
-            p.write(block)
+                p.write(block)
+    except IOError:
+        pass
 
 
 if __name__ == '__main__':
 
-    if user == "esi_id" or password == "esi_pass":
+    if username == "esi_id" or password == "esi_pass":
 
         print("Please enter your credentials. Quitting.")
         exit()
 
 
-    authenticate(user, password, s)
+    authenticate(username, password, s)
     soup, timetable = get_timetable(s)
     pdfs = soup.findAll("a")
 
     for pdf in pdfs:
-        
-        pdf = pdf['href']
+
+        pdf = pdf['href'].split("%2F")[-1]
 
         url = clean_url(timetable)
         url += pdf
 
-        print("Downloading: " + pdf)
-        save_pdf(url, pdf, s)
+        directory = url.split("/")[-2]
+        if not os.path.exists("pdf/" + directory):
+            os.makedirs("pdf/" + directory)
+
+        if url.endswith(".pdf"):
+            print("Downloading: " + pdf + " to pdf/" + directory)
+            save_pdf(url, directory, pdf, s)
 
 
 
